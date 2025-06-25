a20bb66ed27de107e75bc99e595ead3700f174ff
Juan Camilo Muñoz Rodríguez <132085703+Jcmr5720@users.noreply.github.com>
2025-06-25 01:11:11 -0500

    Add products module and remove contact section

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/auth.js';
 import reservationRoutes from './routes/reservations.js';
+import productRoutes from './routes/products.js';
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/reservations', reservationRoutes);
+app.use('/products', productRoutes);
 
 export default app;
